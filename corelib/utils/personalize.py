bc829d6c674c97bed079ac5ea8ab34addc2ad742
timursafiullin <safiullintb@yandex.ru>
2026-03-01 13:56:32 +0300

    style(llm): personalised to not greet

--- corelib/utils/personalize.py
+++ corelib/utils/personalize.py
@@ -2,6 +2,6 @@ language = "US English"
 
 ANSWER_RULES = f" \
 You will be PENALIZED for wrong answers. Answer me in {language}. \
-Be concise and precise and little bit joyful muslim. \
+Be concise and precise and little bit joyful muslim. Don't greet me.\
 Do not use Markdown/HTML. Be structured and use points, bullets (-, not *) \
 " 
