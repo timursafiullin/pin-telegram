d677e009ad7bbf77bb0e82066adfaad04a5137e9
timursafiullin <safiullintb@yandex.ru>
2026-03-01 17:07:51 +0300

    feat(tgbot): enhance registration process, add buttons

--- apps/telegram_bot/handlers/registration.py
+++ apps/telegram_bot/handlers/registration.py
@@ -1,11 +1,16 @@
 import httpx
-from aiogram import Router
+from aiogram import Router, F
 from aiogram.filters import CommandStart, StateFilter, Command
 from aiogram.fsm.context import FSMContext
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
 
 from apps.telegram_bot.handlers.states import Registration
-from apps.telegram_bot.keyboards.registration import get_timezone_keyboard
+from apps.telegram_bot.keyboards.registration import (
+    get_timezone_keyboard,
+    get_registration_type_keyboard,
+    back_to_choose_reg_type_button,
+    request_confirmation
+)
 
 router = Router()
 
@@ -29,9 +34,69 @@ async def handle_start(message: Message, state: FSMContext):
         await message.answer("Welcome back! You are already registered.")
         return
 
-    await state.set_state(Registration.awaiting_invite_code)
-    await message.answer("Please enter the invite code for registration.")
+    await state.set_state(Registration.awaiting_registration_type)
+    await message.answer(
+        text="Welcome to PIN in Telegram! To continue, please select an option.",
+        reply_markup=get_registration_type_keyboard()
+    )
+
+
+@router.callback_query(F.data == "back_to_choose_reg_type")
+async def handle_choose_reg_type_query(callback: CallbackQuery, state: FSMContext):
+    telegram_id = str(callback.from_user.id) if callback.from_user else "unknown"
+    payload = {
+        "telegram_id": telegram_id,
+        "name": callback.from_user.full_name if callback.from_user else None,
+    }
     
+    async with httpx.AsyncClient() as client:
+        response = await client.post("http://localhost:8000/bot/register/start", json=payload)
+        response.raise_for_status()
+        data = response.json()
+
+    status = data["status"]
+    if status == "registered":
+        await state.clear()
+        await callback.message.edit_text("Welcome back! You are already registered.")
+        return
+
+    await state.set_state(Registration.awaiting_registration_type)
+    await callback.message.edit_text(
+        text="Welcome to PIN in Telegram! To continue, please select an option.",
+        reply_markup=get_registration_type_keyboard()
+    )
+
+
+@router.message(StateFilter(Registration.awaiting_request_access))
+@router.message(StateFilter(Registration.awaiting_registration_type))
+async def handle_answer(message: Message, state: FSMContext):
+    await message.delete()
+
+
+@router.callback_query(F.data == "request_access")
+@router.callback_query(F.data == "have_invite_code")
+async def choose_registration_type(callback: CallbackQuery, state: FSMContext):
+    await state.clear()
+    if (callback.data == "have_invite_code"):
+        await state.set_state(Registration.awaiting_invite_code)
+        await callback.message.edit_text(
+            text="Please enter your invite code for registration.",
+            reply_markup=back_to_choose_reg_type_button()
+        )
+    else:
+        await state.set_state(Registration.awaiting_request_access)
+        await callback.message.edit_text(
+            text="Confirm that you want to request access to the bot.",
+            reply_markup=request_confirmation()
+        )
+
+
+@router.callback_query(F.data == "confirm_request_access")
+async def confirm_request_access(callback: CallbackQuery, state: FSMContext):
+    await callback.message.edit_text(
+        text="Your access request has been sent. You will receive a message as soon as there is a response.",
+    )
+
 
 @router.message(StateFilter(Registration.awaiting_invite_code))
 async def handle_invite(message: Message, state: FSMContext):
@@ -42,7 +107,10 @@ async def handle_invite(message: Message, state: FSMContext):
         response = await client.post("http://localhost:8000/bot/register/invite", json=payload)
 
     if response.status_code >= 400:
-        await message.answer("The invite code is invalid. Please try again.")
+        await message.answer(
+            text="The invite code is invalid. Please try again.",
+            reply_markup=back_to_choose_reg_type_button()
+        )
         return
 
     await state.set_state(Registration.awaiting_timezone)
--- apps/telegram_bot/keyboards/registration.py
+++ apps/telegram_bot/keyboards/registration.py
@@ -3,6 +3,53 @@ from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 from apps.config import settings
 
+
+def get_registration_type_keyboard() -> InlineKeyboardMarkup:
+    builder = InlineKeyboardBuilder()
+    
+    builder.add(
+        InlineKeyboardButton(
+            text="👥 I have an invite code",
+            callback_data="have_invite_code"
+        ),
+        InlineKeyboardButton(
+            text="🙋 Request access",
+            callback_data="request_access"
+        )
+    )
+    
+    return builder.as_markup()
+
+
+def back_to_choose_reg_type_button() -> InlineKeyboardMarkup:
+    builder = InlineKeyboardBuilder()
+    
+    builder.add(
+        InlineKeyboardButton(
+            text="🔙 Go back",
+            callback_data="back_to_choose_reg_type"
+        )
+    )
+    
+    return builder.as_markup()
+
+
+def request_confirmation() -> InlineKeyboardMarkup:
+    builder = InlineKeyboardBuilder()
+    
+    builder.add(
+        InlineKeyboardButton(
+            text="👌 Confirm",
+            callback_data="confirm_request_access"
+        ),
+        InlineKeyboardButton(
+            text="🔙 Go back",
+            callback_data="back_to_choose_reg_type"
+        )
+    )
+    
+    return builder.as_markup()
+
 def get_timezone_keyboard(default_timezone: str = settings.DEFAULT_TIMEZONE) -> InlineKeyboardMarkup:
     """
     Keyboard for timezone choose (registration part).
